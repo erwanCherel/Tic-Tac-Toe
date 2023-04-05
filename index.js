5c163fd1dde02ae8737c3abccb4b49685c3cdff1
Wan2302 <erwan.cherel35@hotmail.com>
2023-04-05 13:25:51 +0200

    it works

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-// sur une grille 3 x 3 numérotée :
-// 1 2 3
-// 4 5 6
-// 7 8 9
-// il y a 8 combinaisons de victoire : 
-// 123, 456, 789, 147, 258, 369, 159, 753
+// TO DO: 
+// - ajouter un input pour ajouter son nom;
+// - ajouter un bouton pour restart la partie;
+// - améliorer le resultDisplay;
+// 
 
 
-const Gameboard = (() => {
-    //store the gameboard as an array inside of a Gameboard object
-    let board = [[0, 1, 2], 
-                 [3, 4, 5],
-                 [6, 7, 8]
-                ]
-    return board;
-})()
+const Gameboard = ["", "", "", "", "", "", "", "", ""];
 
 // create players objects
 const players = (player, pattern) => {
@@ -22,35 +14,76 @@ const players = (player, pattern) => {
 const playerOne = players('player one', 'X')
 const playerTwo = players('player two', 'O')
 
-// create & fill the board on the webpage
-const renderArray = (() => {
-    for(let i = 0; i < Gameboard.length; i++) {
-        for (let j = 0; j < Gameboard[i].length; j++) {
-            const cell = document.createElement('div');
-            cell.classList.add('cell')
-            cell.innerHTML = Gameboard[i][j];
-            const gameboard = document.getElementById('gameboard');
-            gameboard.append(cell)
-        }
-       
-    }
-})()
-
 const addMarks = (() => {
-    count = 0;
+    
+    // create the board, allow id's and classes to each cell and fill the gameboard
+    const gameboard = document.getElementById('gameboard');
+    for (let i = 0; i < Gameboard.length; i++) {
+        const cell = document.createElement('div');
+        cell.className = "cell";
+        cell.id = i;
+        gameboard.append(cell)
+    }
+
+    let turn = 0;
     const cells = document.querySelectorAll('.cell');
     cells.forEach((cell) => {
         cell.addEventListener('click', (e) => {
-            if (e.target.classList.contains('cell')) {
-                count += 1;
-                if (count % 2 === 0) {
-                    e.target.innerHTML = "X";
-                    e.target.classList.remove("cell");
+            if (e.target.className === "cell") {
+                turn += 1;
+                if (turn % 2 === 1) {
+                    Gameboard[e.target.id] = playerOne.pattern;
+                    e.target.innerHTML = Gameboard[e.target.id];
+                    e.target.className = "clicked";
+                    checkWinOrTie();
                 } else {
-                    e.target.innerHTML = "O";
-                    e.target.classList.remove("cell");
+                    Gameboard[e.target.id] = playerTwo.pattern;
+                    e.target.innerHTML = Gameboard[e.target.id];
+                    e.target.className = "clicked";
+                    checkWinOrTie()
                 }
             }
         });
     });
+
+    const playAgain = (() => {
+        document.getElementById('cleanGrid').addEventListener('click', () => {
+            turn = 0;
+            document.getElementById('resultDisplay').innerText = "";
+            for(let i = 0; i < Gameboard.length; i++) {
+                Gameboard[i] = "";
+                const clicked = document.querySelectorAll('.clicked');
+                clicked.forEach((click) => {
+                    click.innerHTML = "";
+                    click.className = "cell";
+                })
+            }
+        })
+    })()
+    
 })();
+
+const winningCombos = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
+    [0, 4, 8], [2, 4, 6] // diagonal
+  ];
+  
+  const checkWinOrTie = () => {
+    const resultDisplay = document.getElementById('resultDisplay')
+    for (let combo of winningCombos) {
+      const [a, b, c] = combo;
+      if (Gameboard[a] && (Gameboard[a] === Gameboard[b]) && (Gameboard[a] === Gameboard[c])) {
+        if(Gameboard[a] === playerOne.pattern) {
+            resultDisplay.innerText = "Player one wins!"
+        } else {
+            resultDisplay.innerText = "Player two wins!"
+        }
+        const cells = document.querySelectorAll('.cell');
+            cells.forEach((cell) => {cell.className = "clicked"})
+      }
+    }
+    if (Gameboard.every(cell => cell)) {
+        resultDisplay.innerText = "It's a tie!"
+    }
+  };
